e4fe3e812756b8cd2d0de7dbf7318c074961eb4b
BrianFuraha <maithabfrm@gmail.com>
2025-08-07 15:30:53 +0300

    fix bug

--- password_generator.py
+++ password_generator.py
@@ -42,8 +42,8 @@ while True:
   
   if min_length.isdigit():
     min_length = int(min_length)
-    has_number = input("Do you want to add numbers (y/n)? ").lower()
-    has_special = input("Do you want to add special characters (y/n)? ").lower()
+    has_number = input("Do you want to add numbers (y/n)? ").lower() == "y"
+    has_special = input("Do you want to add special characters (y/n)? ").lower() =="y"
     pwd = generate_password(min_length, has_number, has_special)
     print(pwd)
     break
